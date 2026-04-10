a0209a13902563a0024ec5eec49267d9da75d11a
Tanishk Awasthi <ayushawasthi915@gmail.com>
2026-04-10 14:56:26 +0530

    update

--- devops.py
+++ devops.py
@@ -2,5 +2,5 @@ print("hello dude myself ayush and i am from lucknow")
 a= 45
 b=56
 print(a+b)
-a=input("what is your name and what are you doing here ?")
-print("i am perfect what about you ? ")
+b=input("where is you car ? or you gave to someone ?")
+print("Ohh, that's cool then just go for ride")
