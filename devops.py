c67dabfa2865aa9a74020ea8668cc0a7ef88c553
Tanishk Awasthi <ayushawasthi915@gmail.com>
2026-04-10 13:40:03 +0530

    Fix print statement and add user input prompts

--- devops.py
+++ devops.py
@@ -4,3 +4,6 @@ b=56
 print(a+b)
 a=input("what is your name and what are you doing here ?")
 print("i am perfect what about you ? ")
+
+b=input("where is your car ? huh ? U sold to someone")
+print("Ohh! i knew that thanks god")
